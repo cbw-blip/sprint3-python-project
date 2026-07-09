d8b8c54c933795277dd684e648ac98cb2dc24a38
Cheniz <chenizbobbwallace@gmail.com>
2026-07-08 22:46:19 -0400

    Create task4_loops.py
    Loops and Conditionals

---
+++ task4_loops.py
@@ -0,0 +1,32 @@
+pre_2000_count = 0        
+
+for game in video_game_sales:
+    if game[GLOBAL_SALES] > 25:
+        print(game[NAME], game[GLOBAL_SALES])
+
+for game in video_game_sales: 
+    if game[YEAR] < 2000:
+        pre_2000_count += 1
+print(pre_2000_count)
+
+total_na= 0 
+total_jp=0
+
+for game in video_game_sales:
+     total_na += game[NA_SALES]
+     total_jp += game[JP_SALES]
+if total_na > total_jp:
+    print("North America had higher sales.")
+elif total_jp > total_na:
+    print("Japan had higher sales.")
+else:
+    print("Both regions had equal sales.")
+print (total_na, total_jp)
+
+nintendo_games = []
+
+for game in video_game_sales:
+    if game[PUBLISHER] == "Nintendo":
+       nintendo_games.append(game[NAME])
+print (nintendo_games)
+print( len(nintendo_games))
