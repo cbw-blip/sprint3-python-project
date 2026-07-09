aa909516479868d66d75abf010daec774e3c2b1f
Cheniz <chenizbobbwallace@gmail.com>
2026-07-08 22:35:38 -0400

    Update task1_variables.py
    Variables and Arithmetic

--- task1_variables.py
+++ task1_variables.py
@@ -1,2 +1,9 @@
 total_games = len(video_game_sales)
 print(total_games)
+total_sales = 0
+for game in video_game_sales:
+    total_sales = total_sales + game[GLOBAL_SALES]
+avg_global_sales= total_sales/total_games
+print (f"Average: {avg_global_sales}")
+top_game_share = video_game_sales[0][GLOBAL_SALES] / total_sales * 100
+print(top_game_share)
