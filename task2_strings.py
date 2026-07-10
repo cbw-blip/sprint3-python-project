89b8bf40d4c5e373cf6ac4b9bdbbd9042057eee9
Cheniz <chenizbobbwallace@gmail.com>
2026-07-09 22:02:16 -0400

    Update task2_strings.py

--- task2_strings.py
+++ task2_strings.py
@@ -6,3 +6,12 @@ for name in messy_names:
     print(name.strip( ) .lower( ))
     game = video_game_sales[0]
 print(f"#1 Best Seller: {game[NAME]} ({game[YEAR]}) - ${game[GLOBAL_SALES]}M global sales")
+
+NAME = 1
+YEAR = 3
+GENRE = 4
+PUBLISHER = 5
+NA_SALES = 6
+EU_SALES = 7
+JP_SALES = 8
+GLOBAL_SALES = 9
