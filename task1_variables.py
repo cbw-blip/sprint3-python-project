8570f57e47fb3c623f7550d714165e45b93dcb77
Cheniz <chenizbobbwallace@gmail.com>
2026-07-09 21:53:13 -0400

    Update task1_variables.py
    added setup code

--- task1_variables.py
+++ task1_variables.py
@@ -7,3 +7,15 @@ avg_global_sales= total_sales/total_games
 print (f"Average: {avg_global_sales}")
 top_game_share = video_game_sales[0][GLOBAL_SALES] / total_sales * 100
 print(top_game_share)
+
+
+
+NAME = 1
+YEAR = 3
+GENRE = 4
+PUBLISHER = 5
+NA_SALES = 6
+EU_SALES = 7
+JP_SALES = 8
+GLOBAL_SALES = 9
+
